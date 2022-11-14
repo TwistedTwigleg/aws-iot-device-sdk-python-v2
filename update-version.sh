34a06b5d8d7663a6e1ae8a59262d7fd9b531d365
Noah Beard <ncbeard@amazon.com>
2022-11-14 12:07:53 -0500

    Test making file commit automatically

--- update-version.sh
+++ update-version.sh
@@ -51,21 +51,20 @@ git config --local user.name "TwistedTwigleg"
 
 # --==--
 # NOTE - if you need to make changes BEFORE making a release, do it here and commit the file!
-#new_version_branch=AutoTag-v${new_version}
-#git checkout -b ${new_version_branch}
+new_version_branch=AutoTag-v${new_version}
+git checkout -b ${new_version_branch}
 
-# TODO: make changes to files HERE if needed!
-# NOTE: Do NOT include VERSION file in the commit.
-# Example:
-# git add setup.py
-# git commit -m "Updated version to ${new_version}"
+# NOTE: Make changes to files HERE if needed!
+echo "Wow, a test!" > TEST_OUTPUT
+git add TEST_OUTPUT
+git commit -m "[$new_version] $RELEASE_TITLE"
 
 # push the commit and create a PR
-# git push -u "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/TwistedTwigleg/aws-iot-device-sdk-python-v2.git" ${new_version_branch}
-# gh pr create --title "AutoTag PR for v${new_version}" --body "AutoTag PR for v${new_version}" --head ${new_version_branch}
+git push -u "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/TwistedTwigleg/aws-iot-device-sdk-python-v2.git" ${new_version_branch}
+gh pr create --title "AutoTag PR for v${new_version}" --body "AutoTag PR for v${new_version}" --head ${new_version_branch}
 
 # Merge the PR
-# gh pr merge --admin --squash
+gh pr merge --admin --squash
 # --==--
 
 # Update local state with the merged pr (if one was made) and just generally make sure we're up to date
@@ -82,7 +81,8 @@ git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/TwistedTwigleg/aws-
 # (If a pre-release, then -p needs to be added)
 
 # Create the release with auto-generated notes as the description
-# (NOTE: This will only add stuff if there is at least one PR. If there is no PRs, then this will be blank and need manual input/changing afterwards)
+# - NOTE: This will only add stuff if there is at least one PR. If there is no PRs,
+# - then this will be blank and need manual input/changing after running.
 if [ $IS_PRE_RELEASE == "true" ]; then
     gh release create "v${new_version}" -p --generate-notes --notes-start-tag "$current_version" --target main -t "${RELEASE_TITLE}"
 else
